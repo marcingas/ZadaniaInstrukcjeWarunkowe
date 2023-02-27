d24d75913a114c1cfde11cadbc38b33d20bc3bab
Marcin <gasiorek.mail@gmail.com>
2023-02-27 09:42:12 +0100

    kalkulator rzymski w klasie zadani i zadanie delta w klasie DeltaZadanie

---
+++ src/Zadania.java
@@ -0,0 +1,26 @@
+public class Zadania {
+    public static void main(String[] args) {
+
+       //Kalkulator rzymski:
+
+        String rom [] = {"M","CM","D","CD","C","XC","L","XL","X", "IX", "V", "IV", "I"};
+        int[] arab = {1000,900,500,400,100,90,50,40,10, 9, 5, 4, 1};
+        int number = 1700;
+        StringBuilder sb = new StringBuilder();
+           // while(number > 0) {
+                for(int a = 0; a < arab.length; a++) {
+                   while (number >= arab[a]) {
+                        sb.append(rom[a]);
+                        number -= arab[a];
+                        System.out.println(sb);
+                        System.out.println("index użyty "+a);
+                        System.out.println( "cyfra użyta" + arab[a]);
+                        System.out.println("pozostało" + number);
+                    }
+            }
+
+
+
+
+    }
+}
