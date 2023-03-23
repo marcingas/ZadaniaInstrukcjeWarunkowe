35d91097aa0617deb9e9f91bbb28fd440b85b797
Marcin <gasiorek.mail@gmail.com>
2023-03-23 14:31:40 +0100

    taks 14 first part

--- src/Stream.java
+++ src/Stream.java
@@ -1,7 +1,4 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -10,8 +7,8 @@ public class Stream {
 
 
         Random random = new Random();
-        IntStream intStream = IntStream.generate(()->random.nextInt(100) + 1)
-                        .limit(100);
+        IntStream intStream = IntStream.generate(() -> random.nextInt(100) + 1)
+                .limit(100);
         List<Integer> intList = intStream
                 .boxed()
                 .collect(Collectors.toList());
@@ -19,29 +16,30 @@ public class Stream {
         System.out.println(intList);
 
         long numberOfInts = intList.stream()
-                .filter(x->x % 6 == 0)
+                .filter(x -> x % 6 == 0)
                 .count();
+
         System.out.println("2. ile jest intów podzielnych przez 6: ");
         System.out.println(numberOfInts);
 
         System.out.println("3. pierwszy element podzielny przez 37: ");
-      Optional<Integer> firstDividedByThree = intList.stream()
+        Optional<Integer> firstDividedByThree = intList.stream()
                 .filter(x -> x % 6 == 0)
                 .filter(x -> x % 37 == 0)
                 .findFirst();
-      if(firstDividedByThree.isPresent()){
-          System.out.println(firstDividedByThree);
-      }else{
-          System.out.println("There is no element divided by 37");
-      }
+        if (firstDividedByThree.isPresent()) {
+            System.out.println(firstDividedByThree);
+        } else {
+            System.out.println("There is no element divided by 37");
+        }
         System.out.println("4. Suma wszystkich elementów ");
-      int sum = intList.stream()
-              .mapToInt(Integer::intValue)
-              .sum();
+        int sum = intList.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
         System.out.println("Suma elementów listy " + sum);
 
         System.out.println("5. czy wszystkie liczby są większe od 0 i mniejsze od 100?");
-        boolean istrue  = intList.stream()
+        boolean istrue = intList.stream()
                 .allMatch(x -> x > 0 && x < 100);
         System.out.println("Czy to prawda? " + istrue);
 
@@ -53,29 +51,80 @@ public class Stream {
         System.out.println("Wypisz te liczby: ");
         intList.stream()
                 .distinct()
-                .forEach(x-> System.out.println(x));
+                .forEach(x -> System.out.println(x));
 //       inny sposób wypisania:  .forEach(System.out::println);
+
         System.out.println("7 wypisz sumę unikatowych liczb: ");
 
-       int sumUnique = intList.stream()
-               .distinct()
-               .mapToInt(Integer::intValue)
-               .sum();
+        int sumUnique = intList.stream()
+                .distinct()
+                .mapToInt(Integer::intValue)
+                .sum();
         System.out.println("Suma unikatowych liczb to " + sumUnique);
 
         System.out.println("8.usuń z listy liczby podzielne przez 3 i wypisz listę:");
-        intList.stream()
+        List<Integer> shorterList = intList.stream()
                 .filter(x -> x % 3 != 0)
-                .collect(Collectors.toList())//collect - i collectors.toList tworzą nową listę wybranych elementów.
-                .forEach(System.out::println);
+                .collect(Collectors.toList());//collect - i collectors.toList tworzą nową listę wybranych elementów.
+        System.out.println(shorterList);
 
         System.out.println("9. liczby o indeksach 15-30 włącznie:");
 
-        intList.stream()
+        List<Integer> indexList = intList.stream()
                 .skip(15)
                 .limit(16)
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
-        System.out.println("10. zamień inty na Stringi");
+                .collect(Collectors.toList());
+        System.out.println(indexList);
+
+        System.out.println("10. zamień inty na Stringi:");
+        List<String> stringList = intList.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+        System.out.println(stringList);
+
+        System.out.println("11. posortuj listę: ");
+        List<Integer> sortedList = intList.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(sortedList);
+
+        System.out.println("W odwrotnej kolejności:");
+        List<Integer> reverseList = intList.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        System.out.println(reverseList);
+
+        System.out.println("12. Wypisz największą wartość w liście:");
+        Optional max = intList.stream()
+                .max(Integer::compareTo);
+        System.out.println(max);
+
+        System.out.println("13. Wypisz najmniejszą wartość w liście:");
+        Optional min = intList.stream()
+                .min(Integer::compareTo);
+        System.out.println(min);
+
+        System.out.println("14. zamień listę na set:");
+        Set<Integer>setInt = intList.stream()
+                .collect(Collectors.toSet());
+        System.out.println(setInt);
+        Set<Integer>setInt2 = intList.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toSet());
+
+        System.out.println("Zadanie 15");
+        Random random1 = new Random();
+        List<String> randomString = intList.stream()
+                .map(num->{
+                    StringBuilder sb = new StringBuilder();
+                    for(int i = 0; i < num;i++){
+                        char c = (char)(random1.nextInt(26) + 'a');
+                        sb.append(c);
+                    }
+                    return sb.toString();
+                }).collect(Collectors.toList());
+        System.out.println(randomString);
+
+
     }
 }
